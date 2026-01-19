db808c0e5ce47458634203fade79c5a8ccbda765
IviLegend <elivilegend@gmail.com>
2026-01-19 13:43:24 +0100

    Package partidas y getters y setters

--- src/main/java/org/example/quizzen/partidas/GestorPartida.java
+++ src/main/java/org/example/quizzen/partidas/GestorPartida.java
@@ -1,9 +1,18 @@
 package org.example.quizzen.partidas;
 
+import org.example.quizzen.preguntas.Pregunta;
+
+import java.util.ArrayList;
+
 public class GestorPartida
 {
     private Partida partida;
     private int indicePregunta;
 
+    ArrayList<Pregunta> preguntas = partida.getTest().getPreguntas();
 
+    public void responderPregunta()
+    {
+
+    }
 }
--- src/main/java/org/example/quizzen/partidas/Partida.java
+++ src/main/java/org/example/quizzen/partidas/Partida.java
@@ -10,4 +10,34 @@ public class Partida
     //private Usuario usuario;
     private LocalDateTime fechaPartida;
     private Resultados resultados;
+
+    public Test getTest()
+    {
+        return test;
+    }
+
+    public void setTest(Test test)
+    {
+        this.test = test;
+    }
+
+    public LocalDateTime getFechaPartida()
+    {
+        return fechaPartida;
+    }
+
+    public void setFechaPartida(LocalDateTime fechaPartida)
+    {
+        this.fechaPartida = fechaPartida;
+    }
+
+    public Resultados getResultados()
+    {
+        return resultados;
+    }
+
+    public void setResultados(Resultados resultados)
+    {
+        this.resultados = resultados;
+    }
 }

14787c93720a799075142fc750559098c90b3f6a
alumno <nicoletaagrigoras@gmail.com>
2026-01-19 14:15:19 +0100

    Resultados realizado, pero hace falta echarle una revision por si falta alguna función

--- src/main/java/org/example/quizzen/partidas/Resultados.java
+++ src/main/java/org/example/quizzen/partidas/Resultados.java
@@ -1,5 +1,26 @@
 package org.example.quizzen.partidas;
 
+import org.example.quizzen.preguntas.Pregunta;
+
+import java.util.Map;
+
 public class Resultados
 {
+    public Map<Pregunta,String> respuestas;
+
+    public Resultados()
+    {
+
+    }
+
+    public void anyadirRespuestaFallada(Pregunta preguntaActual, String respuestaUsuario)
+    {
+        this.respuestas.put(preguntaActual, respuestaUsuario);
+    }
+
+    public int totalRespuestasFalladas()
+    {
+        return respuestas.size();
+    }
 }
+
--- src/main/java/org/example/quizzen/preguntas/PreguntaOpcionMultiple.java
+++ src/main/java/org/example/quizzen/preguntas/PreguntaOpcionMultiple.java
@@ -2,7 +2,7 @@ package org.example.quizzen.preguntas;
 
 import java.util.ArrayList;
 
-public class PreguntaOpcionMultiple
+public class PreguntaOpcionMultiple extends Pregunta
 {
     private ArrayList<Opcion> opciones;
 }

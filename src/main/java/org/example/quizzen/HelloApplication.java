6e72a173347264b0ae7b2cf3f32066c98bb57b7a
alumno <alvaroseguradelbarrio@gmail.com>
2026-01-27 11:05:58 +0100

    Se ha añadido un botón

--- src/main/java/org/example/quizzen/HelloApplication.java
+++ src/main/java/org/example/quizzen/HelloApplication.java
@@ -1,11 +1,15 @@
 package org.example.quizzen;
 
+import javafx.animation.Interpolator;
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.example.quizzen.preguntas.*;
 
 import java.io.IOException;
@@ -21,9 +25,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        //Hacer un boton para ir a hacer las preguntas.
 
-        //
 
 
 
@@ -34,13 +36,39 @@ public class HelloApplication extends Application {
         listaPreguntas= muchasPreguntas();
 
         root = new BorderPane();
+        root.setStyle("-fx-background-color: #0f172a;");
         Scene scene = new Scene(root, 800, 600);
         stage.setScene(scene);
         stage.show();
 
-        mostrarPregunta(stage);
+        //Hacer un boton para ir a hacer las preguntas.
+        Button btonPreguntas = new Button("Hacer Test");
+        btonPreguntas.setPrefWidth(500);
+        btonPreguntas.setPrefHeight(500);
+
+        String estiloBoton =
+                "-fx-background-color: linear-gradient(to bottom right, #d8b4fe, #7c3aed);" +
+                        "-fx-background-radius: 10;" +
+                        "-fx-padding: 10 30 10 30;" +
+                        "-fx-text-fill: white;" +
+                        "-fx-font-weight: bold;" +
+                        "-fx-font-size: 40px;";
+
+        btonPreguntas.setStyle(estiloBoton);
+
+
+        cambiarTamanyoBoton(btonPreguntas,1.25,1.0);
+
+        btonPreguntas.setOnMouseClicked(e -> mostrarPregunta(stage));
+        root.setCenter(btonPreguntas);
+        //
+
+
+        //mostrarPregunta(stage);
 
     }
+
+
 
     private void mostrarPregunta(Stage stage){
 
@@ -75,6 +103,23 @@ public class HelloApplication extends Application {
             mostrarPregunta(stage);
         }
 
+    }
+
+    private static void cambiarTamanyoBoton(Button unBotonCualquiera, double tamanyoGrande, double tamanyoNormal) {
+        ScaleTransition hacerBotonGrande = new ScaleTransition(Duration.millis(200), unBotonCualquiera);
+        hacerBotonGrande.setToX(tamanyoGrande);
+        hacerBotonGrande.setToY(tamanyoGrande);
+        hacerBotonGrande.setInterpolator(Interpolator.EASE_OUT);
+
+        ScaleTransition botonTamanyoNormal = new ScaleTransition(Duration.millis(200), unBotonCualquiera);
+        botonTamanyoNormal.setToX(tamanyoNormal);
+        botonTamanyoNormal.setToY(tamanyoNormal);
+        botonTamanyoNormal.setInterpolator(Interpolator.EASE_BOTH);
+
+        unBotonCualquiera.setOnMouseEntered(e ->
+                hacerBotonGrande.playFromStart());
+        unBotonCualquiera.setOnMouseExited(e ->
+                botonTamanyoNormal.playFromStart());
     }
 
     private static ArrayList<Pregunta> muchasPreguntas(){
